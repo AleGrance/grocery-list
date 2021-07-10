5c2ced16948d6997f1c57092f08cdfa6d4691f9b
Ale Grance <alegrance1000@gmail.com>
2021-07-10 14:24:04 -0400

    Show container after add item

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,14 @@
 const form = document.querySelector(".grocery-form");
 const submitBtn = document.querySelector(".submit-btn");
 const alert = document.querySelector(".alert");
+const groceryContainer = document.querySelector(".grocery-container");
 
 const grocery = document.getElementById("itemTitle");
+const groceryList = document.getElementById("grocery-list");
 
 
 // Add Event Listeners
 form.addEventListener("submit", addItem);
-
 
 
 
@@ -18,15 +19,43 @@ function addItem(e) {
     // Prevent page reloading
     e.preventDefault();
 
+    // Set the item values
     const value = grocery.value;
     const id = new Date().getTime().toString();
 
     console.log(id, value);
 
+    // Call the function that creates the itemRow
+    createItem(id, value);
+
     showAlert("Agregado!", "success");
 }
 
+// Show alert
 function showAlert(textContent, type) {
     alert.textContent = textContent;
     alert.classList.add(type);
+}
+
+// Create Item Row with the item values
+function createItem(id, value) {
+    // Create a new element
+    const element = document.createElement("div");
+
+    // Set the element attributes
+    element.classList.add("itemRow");
+    element.setAttribute("id", id);
+
+    // Add elements to the element
+    element.innerHTML = `
+    <article>${value}</article>
+    <div class="btn-container">
+        <button class="btn-delete" type="button"><i class="fa fa-trash"></i></button>
+        <button class="btn-edit" type="button"><i class="fa fa-edit"></i></button>
+    </div>
+    `
+    // Add the element created to the Grocery list DOM
+    groceryList.appendChild(element);
+
+    groceryContainer.classList.add("show-container");
 }
